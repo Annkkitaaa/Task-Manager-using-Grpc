862bf95fc8610914b94a3de76870576701ed2e0f
Ankita Singh <ankitasingh15.102@gmail.com>
2024-04-10 11:35:32 +0530

    Update integration_test.go

--- TaskMaster/tests/integration_test.go
+++ TaskMaster/tests/integration_test.go
@@ -11,8 +11,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/JyotinderSingh/task-queue/pkg/common"
-	pb "github.com/JyotinderSingh/task-queue/pkg/grpcapi"
+	"github.com/Annkkitaaa/task-manager-using-grpc/TaskMaster/pkg/common"
+	pb "github.com/Annkkitaaa/task-manager-using-grpc/TaskMaster/pkg/grpcapi"
 	"google.golang.org/grpc"
 )
 
